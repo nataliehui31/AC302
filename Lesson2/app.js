04bdb9ccd4911e856a9326d9ed401e6c9be09653
nataliehui31 <nataliehui31@gmail.com>
2020-07-28 14:31:14 +0800

    first push

---
+++ Lesson2/app.js
@@ -0,0 +1,18 @@
+console.log("test..."); 
+
+var c = document.getElementById('myCanvas');
+var ctx = c.getContext('2d');
+ctx.moveTo(0,10)
+ctx.lineTo(100,50);
+ctx.strokeStyle = 'blue';
+ctx.lineWidth = 5;
+
+ctx.fillStyle = 'rgb( 200, 50, 10)';
+ctx.fillRect(75, 125, 150, 100); 
+
+ctx.strokeStyle = 'green'
+ctx.strokeRect(50, 100, 200, 150);
+
+ctx.clearRect(125, 150, 50, 50);
+
+ctx.stroke();
